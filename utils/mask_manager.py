f0644af5d04625933e1ae2e37059d56a86a6e460
pachoning <cc.pachon@gmail.com>
2022-04-14 15:54:28 +0000

    Delete unnecessary attributes

--- utils/mask_manager.py
+++ utils/mask_manager.py
@@ -2,15 +2,8 @@ import os
 from skimage import segmentation, io
 
 class MaskManager:
-    
-    def __init__(self, location, image_name):
-        self.location = location
-        self.image_name = image_name
-        
-    def get_mask(self, kernel_size, max_dist, ratio):
-        
-        full_path = os.path.join(self.location, self.image_name)
-        img = io.imread(full_path)
+
+    def get_mask(self, img, kernel_size, max_dist, ratio):
+
         mask = segmentation.quickshift(img, kernel_size=kernel_size, max_dist=max_dist, ratio=ratio)
-            
         return mask
