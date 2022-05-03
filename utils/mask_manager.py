98b3e9d31ac88d2394ca2625ba464abe9040f40e
pachoning <cc.pachon@gmail.com>
2022-05-03 15:52:24 +0000

    Add random_seed to mask_manager

--- utils/mask_manager.py
+++ utils/mask_manager.py
@@ -3,7 +3,8 @@ from skimage import segmentation, io
 
 class MaskManager:
 
-    def get_mask(self, img, kernel_size, max_dist, ratio):
+    def get_mask(self, img, kernel_size, max_dist, ratio, random_seed):
 
-        mask = segmentation.quickshift(img, kernel_size=kernel_size, max_dist=max_dist, ratio=ratio)
+        mask = segmentation.quickshift(img, kernel_size=kernel_size, max_dist=max_dist,
+                                       ratio=ratio, random_seed=random_seed)
         return mask
